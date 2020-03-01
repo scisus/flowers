39e84baa680e023e87ed0b91ea2b4c0ecfe2f91f
Susannah Tysor <susannah@pobox.com>
2020-03-01 07:00:12 -0800

    Added binary data

--- data-raw/phenologydata.R
+++ data-raw/phenologydata.R
@@ -1,6 +1,8 @@
 ## code to prepare `phenologydata` dataset goes here
 
+library(usethis)
+
 phenology <- read.csv("data-raw/inferred_derived_phenology.csv", stringsAsFactors = FALSE, header = TRUE)
 
-usethis::use_data("phenology")
+usethis::use_data(phenology, overwrite = TRUE)
 
