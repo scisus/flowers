2688a36b412489646a9632cf93af7cd315ca64d0
Susannah <susannah.tysor@gmail.com>
2021-05-10 10:48:33 -0700

    use derived instead of inferred and derived phenology data

--- data-raw/phenologydata.R
+++ data-raw/phenologydata.R
@@ -2,7 +2,7 @@
 
 library(usethis)
 
-phenology <- read.csv("data-raw/inferred_derived_phenology.csv", stringsAsFactors = FALSE, header = TRUE)
+phenology <- read.csv("data-raw/derived_phenology.csv", stringsAsFactors = FALSE, header = TRUE)
 
 usethis::use_data(phenology, overwrite = TRUE)
 
